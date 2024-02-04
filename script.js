0b773268b8129361a0d06408afb006a626825671
Anshi-01 <anshisrivastav16@gmail.com>
2024-02-04 15:44:48 +0530

    updated

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var startbtn=document.querySelector("button")
 var allElem=document.querySelectorAll(".elemContainer")
 var playground= document.querySelector("#playground")
 var selected=""
+var scoreVal=document.querySelector("#nav h5 span")
+var score=0
+var timeVal=document.querySelector("#nav h4 span")
+var time=0
+var timer
+var resultBar=document.querySelector(".time-score")
+var result=document.querySelector(".time-score h5")
+
 
 startbtn.addEventListener("click",function(){
     screen[1].style.transform="translateY(-100%)"
@@ -12,15 +20,16 @@ allElem.forEach(function(val){
     val.addEventListener("click",function(){
         screen[2].style.transform="translateY(-200%)"
 
-        var selected = val.childNodes[3].getAttribute("src")
-        setTimeout(function(){
-            createImg(selected)
-        },2000)
+     selected = val.childNodes[3].getAttribute("src")
+     
+       timer= setInterval(function(){
+        createImg()
+       },1000)
       
 })
 })
 
-function createImg(selected){
+function createImg(){
     var newImg= document.createElement("img")
 
     var obj= getRandom()
@@ -29,16 +38,50 @@ function createImg(selected){
     newImg.style.rotate = obj.rot+"deg"
     
     newImg.setAttribute("src",selected)
+     playground.appendChild(newImg) 
 
-     playground.appendChild(newImg)    
+     catchImg()
+
+     setTimeout(function(){
+       playground.removeChild(newImg)
+    },1500)   
  
 }
 
 
  function getRandom(){
-   var x= Math.floor(Math.random()*100)
-   var y= Math.floor(Math.random()*100)
-   var rot= Math.floor(Math.random()*360)
+    var x= Math.floor(Math.random()*100)
+    var y= Math.floor(Math.random()*100)
+    var rot= Math.floor(Math.random()*360)
 
     return{x,y,rot}
  }
+
+ function catchImg(){
+    var images =document.querySelectorAll("#playground img")
+   
+    images.forEach(function(elem){
+        elem.addEventListener("click",function(){
+           score=score+1
+            scoreVal.innerHTML=score
+        })
+    })
+
+    setTimeout(() => {
+       time=time+1
+        timeVal.innerHTML=time
+    }, 1000);
+
+
+    setTimeout(() => {
+       clearInterval(timer)
+       resultBar.style.display="initial"
+       result.innerHTML=`Game Over!\n Your Score : ${score}`
+
+       
+
+    }, 29000);
+
+ }
+
+
