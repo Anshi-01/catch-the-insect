49d4ae44c391a3a3fb1df37dc8710c01a051b9a4
Anshi-01 <anshisrivastav16@gmail.com>
2024-02-16 16:19:42 +0530

    updated

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ var selected=""
 var scoreVal=document.querySelector("#nav h5 span")
 var score=0
 var timeVal=document.querySelector("#nav h4 span")
-var time=0
-var timer
-var resultBar=document.querySelector(".time-score")
-var result=document.querySelector(".time-score h5")
+var min=0;
+var sec=0;
+var time;
+var resultBar=document.querySelector("#playground h3")
+var creating; 
 
 
 startbtn.addEventListener("click",function(){
@@ -22,25 +23,28 @@ allElem.forEach(function(val){
 
      selected = val.childNodes[3].getAttribute("src")
      
-       timer= setInterval(function(){
+      creating= setInterval(function(){
         createImg()
        },1000)
+       timeIncrease()
       
 })
 })
 
 function createImg(){
     var newImg= document.createElement("img")
-
-    var obj= getRandom()
-    newImg.style.left = obj.x+"%"
-    newImg.style.top = obj.y+"%"
-    newImg.style.rotate = obj.rot+"deg"
-    
+    const{x,y,rot}= getRandom()
     newImg.setAttribute("src",selected)
-     playground.appendChild(newImg) 
+    newImg.style.left = x+"px"
+    newImg.style.top = y+"px"
+    newImg.style.rotate = rot+"deg"
 
-     catchImg()
+    newImg.addEventListener("click",function(){
+        catchImg()
+        newImg.style.cursor="grabbing"
+    })
+    
+     playground.appendChild(newImg) 
 
      setTimeout(function(){
        playground.removeChild(newImg)
@@ -50,39 +54,50 @@ function createImg(){
 
 
  function getRandom(){
-    var x= Math.floor(Math.random()*100)
-    var y= Math.floor(Math.random()*100)
+    var x= Math.random()*(window.innerWidth - 200)+100
+    var y= Math.random()*(window.innerHeight - 200)+100
     var rot= Math.floor(Math.random()*360)
 
     return{x,y,rot}
  }
 
+ 
  function catchImg(){
-    var images =document.querySelectorAll("#playground img")
-   
-    images.forEach(function(elem){
-        elem.addEventListener("click",function(){
-           score++
-            scoreVal.innerHTML=score
-        })
-      
-    })
-
-    setTimeout(() => {
-       time++
-        timeVal.innerHTML=time
-    }, 1000);
+    increaseScore()  
+    addImages()      
+    }
 
 
-    setTimeout(() => {
-       clearInterval(timer)
-       resultBar.style.display="initial"
-       result.innerHTML=`Game Over!\n Your Score : ${score}`
-
-       
-
-    }, 29000);
-
- }
+    function addImages(){
+        setTimeout(createImg,1000)
+        setTimeout(createImg,1000)
+    }
 
 
+    function increaseScore(){
+        score++
+        scoreVal.innerHTML=score
+    }
+
+
+   function timeIncrease(){
+   time= setInterval(function(){
+        sec++
+        if(sec==60){
+            sec=0;
+            min++
+        }
+        timeVal.innerHTML=`${min} : ${sec}`
+        if(min==1 && sec==0){
+            clearInterval(time)
+            clearInterval(creating)
+            gameOver()
+        }
+    },1000)
+   }
+
+
+   function gameOver(){
+    resultBar.style.display="block"
+    resultBar.innerHTML=`Game Over ! <br> Your Score : ${score}`
+   }
